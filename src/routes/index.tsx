6e51b50ff2645c3c74ee8c437d6e904a8270da6e
toazthom <109962138+toazthom@users.noreply.github.com>
2022-11-30 16:24:57 -0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <a href="https://start.solidjs.com" target="_blank">
           start.solidjs.com
         </a>{" "}
-        to learn how to build SolidStart apps.
+        to learn how Thomas defeated the Mongoals!.
       </p>
     </main>
   );
