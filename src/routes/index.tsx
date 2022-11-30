81e7e345dc4552282bbcbc0061f42c43ce2c93fc
toazthom <109962138+toazthom@users.noreply.github.com>
2022-11-30 16:23:43 -0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <Title>Thomas is amazing and can build websites now! Yeet.</Title>
-      <h1>Hello world!</h1>
+      <h1>Thomas is amazing and can build websites now! Yeet.</h1>
       <Counter />
       <p>
         Visit{" "}
