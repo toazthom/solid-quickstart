ee7ed94c135731e38de7d30e6c8ded83f59e2899
toazthom <109962138+toazthom@users.noreply.github.com>
2022-12-01 16:43:19 -0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,7 @@ export default function Home() {
       <h1>Thomas is amazing and can build websites now! Yeet.</h1>
       <Counter />
       <p>
+        <img src="mmmm, pupy!!!.jpg" alt="Italian Trulli">
         Visit{" "}
         <a href="https://github.com/toazthom/solid-quickstart/blob/main/mmmm%2C%20pupy!!!.jpg">
           start.solidjs.com
