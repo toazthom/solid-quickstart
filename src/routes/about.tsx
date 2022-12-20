5b9632cb275eac90381041c0fd3481bf218c74fc
toazthom <109962138+toazthom@users.noreply.github.com>
2022-12-20 12:37:15 -0500

    Update about.tsx

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -2,6 +2,29 @@
 import { Title } from "solid-start";
 import Counter from "~/components/Counter";
 
+
+export function MyComponent() {
+  const [_, { Form }] = createRouteAction(async (formData: FormData) => {
+    await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+    const username = formData.get("username");
+    if (username === "admin") {
+      return redirect("/admin");
+    } else {
+      throw new Error("Invalid username");
+    }
+    return redirect("/home");
+  });
+ 
+  return (
+    <Form>
+      <label for="username">Username:</label>
+      <input type="text" name="username" />
+      <input type="submit" value="submit" />
+    </Form>
+  );
+}
+
+
 export default function Home() {
   return (
     <main>
