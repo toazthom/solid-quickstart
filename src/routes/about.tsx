b9ab17d9f6fc263bad7db97dc4bd72f14edb81b7
toazthom <109962138+toazthom@users.noreply.github.com>
2022-12-20 13:11:24 -0500

    Update about.tsx

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -35,12 +35,12 @@ export default function Home() {
  
       <h2>Text input fields</h2>
 
-<form>
-  <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" value="John"><br>
-  <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" value="Doe">
-</form>
+      <form>
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname" value="John"><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname" value="Doe">
+      </form>
       
     
     
