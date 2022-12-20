480a3e5edfda5b44936d2ddecdb2baeb641d91dc
toazthom <109962138+toazthom@users.noreply.github.com>
2022-12-20 12:31:56 -0500

    Create about.tsx

---
+++ src/routes/about.tsx
@@ -0,0 +1,21 @@
+
+import { Title } from "solid-start";
+import Counter from "~/components/Counter";
+
+export default function Home() {
+  return (
+    <main>
+      
+      <Title>Paired Comparison Program</Title>
+      <h1>Thomas is amazing and can build websites now! Yeet.</h1>
+      <Counter />
+      <p>
+        Visit{" "}
+        <a href="https://github.com/toazthom/solid-quickstart/blob/main/mmmm%2C%20pupy!!!.jpg">
+          start.solidjs.com
+        </a>{" "}
+        to learn how Thomas defeated the Mongoals!.
+      </p>
+    </main>
+  );
+}
