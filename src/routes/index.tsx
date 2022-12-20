1bab5889c0c3a5552e92b3baac3579cca516fc17
toazthom <109962138+toazthom@users.noreply.github.com>
2022-12-20 12:24:00 -0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       
       <Title>Paired Comparison Program</Title>
       <h1>Thomas is amazing and can build websites now! Yeet.</h1>
-      
+      <Counter />
       <p>
         Visit{" "}
         <a href="https://github.com/toazthom/solid-quickstart/blob/main/mmmm%2C%20pupy!!!.jpg">
