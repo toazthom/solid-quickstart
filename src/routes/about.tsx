8e83124705ed3457c5a85bcbbfb825d239c140bc
toazthom <109962138+toazthom@users.noreply.github.com>
2022-12-20 12:56:57 -0500

    Update about.tsx

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -13,6 +13,16 @@ export default function Home() {
       <h1>Insert essays and their ID numbers here.</h1>
 
       
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
       <table>
         <tr>
           <th>Company</th>
@@ -31,7 +41,20 @@ export default function Home() {
         </tr>
       </table>      
 
+      
+      
+      
+      
+      
+      <h2>HTML Forms</h2>
 
+      <form action="/action_page.php">
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname" value="John"><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname" value="Doe"><br><br>
+        <input type="submit" value="Submit">
+      </form> 
       
     
     
