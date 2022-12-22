6e69942ee69091bed301a1ea06e3f9f6dacdba6e
toazthom <109962138+toazthom@users.noreply.github.com>
2022-12-22 12:39:46 -0500

    Update about.tsx

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -33,16 +33,6 @@ export default function Home() {
       </table>      
 
 
-
-      <p>Note that the form itself is not visible.</p>
-
-      <p>Also note that the default width of text input fields is 20 characters.</p>
-
-      </body>
-      </html>
-
-      
-    
     
       <Counter />
       <p>
