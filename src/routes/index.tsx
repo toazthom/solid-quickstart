9fe4b7b102914b778f20a8cb0ed4e894b232c513
toazthom <109962138+toazthom@users.noreply.github.com>
2023-04-21 22:22:22 -0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,28 @@ export default function Home() {
       
       <Title>Paired Comparison Program</Title>
       <h1>Thomas is amazing and can build websites now! Yeet.</h1>
+      <!--Form Code--> 
+<form action=”mailto:contact@yourdomain.com”
+
+
+method=”POST”
+
+
+enctype=”text/plain”
+
+
+name=”EmailForm”>
+
+
+<label for="name">Name:</label><br>
+<input type="text" id="name" name="name"><br>
+
+
+<label for="ContactCommentt">Message:</label><br>
+<textarea id=”ContactCommentt” rows=”6″ cols=”20″>
+
+<input type="submit" value="Send">
+</form>
       <Counter />
       <p>
         Visit{" "}
